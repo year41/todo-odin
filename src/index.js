284e224398019e59ad7b7bb295f46023fe8a5fc2
qwerty <162951673+year41@users.noreply.github.com>
2026-07-03 18:00:51 +1000

    Create "todo" class-object constructor.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import "./styles.css";
 
-console.log("JavaScript Console Working")
+const todoList = [];
+const todoProjects = [];
+
+class todoItem {
+    constructor(title, dueDate, description, priority) {
+        this.id = crypto.randomUUID();
+        this.title = title;
+        this.dueDate = dueDate;
+        this.description = description;
+        this.priority = priority;
+    };
+
+    priorityState() {
+        if (this.priority === true) {
+            return this.priority = false;
+        } else {
+            return this.priority = true;
+        };
+    };
+}
